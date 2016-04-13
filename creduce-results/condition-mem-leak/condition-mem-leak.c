0744e03f74664cdaff129580f20a41dfee3d0900
Zachary <zpreynol@iupui.edu>
2016-04-13 11:04:14 -0400

    Corrected condition-mem-leak.c

--- creduce-results/condition-mem-leak/condition-mem-leak.c
+++ creduce-results/condition-mem-leak/condition-mem-leak.c
@@ -1,21 +1,25 @@
-// Condition - memory leak
-
-#include <stdio.h>
 #include <wchar.h>
-#include <stdlib.h>
-/* The two variables below are not defined as "const", but are never
-   assigned any other value, so a tool should be able to identify that
-   reads of these will always return their initialized values. */
-static int staticTrue = 1;
-
-
-int main(void) {
+#define BAD_SOURCE_FIXED_STRING L"Fixed String" 
+#define SEARCH_CHAR L'S'
+static int staticTrue = 1; 
+static void goodB2G2()
+{
     wchar_t * data;
     data = (wchar_t *)malloc(100*sizeof(wchar_t));
-    wcscpy(data, L"This is some data");
-    if (staticTrue) {
-        free(data);
+    data[0] = L'\0';
+    wcscpy(data, BAD_SOURCE_FIXED_STRING);
+    if(staticTrue)
+    {
+        {
+            free(data);
+        }
     }
-    /* FP: Memory leak: data */
-    // `data` is properly released since the condition clearly evaluates to true.
+}
+int main(int argc, char * argv[])
+{
+    srand( (unsigned)time(NULL) );
+    printLine("Calling good()...");
+    CWE761_Free_Pointer_Not_at_Start_of_Buffer__wchar_t_fixed_string_05_good();
+    printLine("Finished good()");
+    return 0;
 }
