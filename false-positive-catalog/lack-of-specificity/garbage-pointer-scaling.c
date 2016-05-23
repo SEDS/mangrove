8aaa47a85879751f9c96d78d7d4498d0edcbcb0c
Zachary <zpreynol@iupui.edu>
2016-05-23 08:49:38 -0400

    Added lack-of-specificity garbage-pointer-scaling FP

---
+++ false-positive-catalog/lack-of-specificity/garbage-pointer-scaling.c
@@ -0,0 +1,18 @@
+// Juliet CWE468_Incorrect_Pointer_Scaling__char_ptr_to_int_01.c
+// Pattern: lack-of-specificity/garbage-pointer-scaling
+
+#include <stdio.h>
+
+int main(void)
+{
+    int intArray[5] = { 1, 2, 3, 4, 5 };
+    char * charPointer = (char *)intArray;
+
+    // intend to read intArray[2] 
+    // FLAW: sizeof() needed since pointer is a char*, not an int*
+
+    // scan-build FP: (warning) Assigned value is garbage or undefined
+    // Cppcheck FP: None
+    int toPrint = (int) (*(charPointer + 2));
+    printf("intArray[2]: %d\n", toPrint);
+}
