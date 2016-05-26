2dfff4e7d8559df27929ce1f1672f8182ac8bfb4
Zachary <zpreynol@iupui.edu>
2016-05-26 14:25:26 -0400

    Added variant to condition-external-const-var FP structure

---
+++ false-positive-catalog/condition-external-const-var/condition-external-const-var_01.cpp
@@ -0,0 +1,27 @@
+// Juliet CWE588_Attempt_to_Access_Child_of_Non_Structure_Pointer__class_09.cpp
+// Parent structure: condition-external-const-var
+
+// #################################################################
+// ## Variant: condition-external-const-var_01
+// ## CHANGE: Condition variable defined within this file
+// #################################################################
+
+#include <stdio.h>
+
+// CHANGE: Define the variable within this file instead of externally.
+const int GLOBAL_CONST_TRUE = 1;
+
+int main(void)
+{
+    int * intPtr;
+    // Simplified the data structure from a class in Juliet to an integer.
+    int intVar = 7;
+    if(GLOBAL_CONST_TRUE)
+    {
+        intPtr = &intVar;
+    }
+
+    // scan-build FP: none
+    // Cppcheck FP: none
+    printf("int: %d\n", *intPtr);
+}
