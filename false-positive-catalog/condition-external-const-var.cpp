f4792a09a4f4308abad972314c297c662f795637
Zachary <zpreynol@iupui.edu>
2016-05-26 11:35:11 -0400

    Revised comments in condition-external-const-var.cpp FP structure

--- false-positive-catalog/condition-external-const-var.cpp
+++ false-positive-catalog/condition-external-const-var.cpp
@@ -1,13 +1,15 @@
 // Juliet CWE588_Attempt_to_Access_Child_of_Non_Structure_Pointer__class_09.cpp
 // Pattern: condition-external-const-var
 
+// *** REQUIRES TEST CASE SUPPORT FILES ***
+
 // This pattern is a combination of "Global Variables" category and "Externally Defined Symbols" category.
-// If global variable is defined locally, the FPs go away (for scan-build and Cppcheck)
+// If the global variable is defined within this file (still globally), the FPs go away (for scan-build and Cppcheck).
 
 #include "std_testcase.h"
 #include <stdio.h>
 
-// GLOBAL_CONST_TRUE defined as follows in io.c
+// GLOBAL_CONST_TRUE is defined as follows in io.c
 // const int GLOBAL_CONST_TRUE = 1;
 
 int main(void)
