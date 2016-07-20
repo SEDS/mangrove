d325b83b11d148cfa1fac2cef3d2d11fb15d3ae5
Zach Reynolds <zpreynol@iupui.edu>
2016-07-20 14:04:43 -0400

    Added consecutive-token FP pattern

---
+++ false-positive-catalog/consecutive-token.cpp
@@ -0,0 +1,18 @@
+int helper (void) {
+    bool flag = false;
+    for (int i = 0; i < 10; i++) {
+        if (flag) {
+            // CodeSonar FP: Unreachable Data Flow. The highlighted code will not execute under any circumstances. This may be because of: (1) A function call that does not return, (2) a test whose result is always the same (look for a preceding Redundant Condition warning), (3) a crashing bug (look for a preceding Null Pointer Dereference or Division By Zero warning).
+            return 1;
+        }
+        flag = true;
+    }
+    // Note: CodeSonar gives a TP Unreachable Data Flow warning here.
+    return 0;
+}
+
+int main(void) {
+    // CodeSonar FP: Unreachable Data Flow. The highlighted code will not execute under any circumstances. This may be because of: (1) A function call that does not return, (2) a test whose result is always the same (look for a preceding Redundant Condition warning), (3) a crashing bug (look for a preceding Null Pointer Dereference or Division By Zero warning).
+    int result = helper();
+    return 0;
+}
