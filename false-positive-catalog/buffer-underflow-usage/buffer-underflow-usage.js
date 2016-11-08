dffa7592bd45922b814128dc3cf45a5b8c0d0948
Zachary <zpreynol@iupui.edu>
2016-11-07 20:33:08 -0500

    Added note for flawed version

--- false-positive-catalog/buffer-underflow-usage/buffer-underflow-usage.js
+++ false-positive-catalog/buffer-underflow-usage/buffer-underflow-usage.js
@@ -11,9 +11,15 @@ var buffer = Buffer.alloc(BUFFER_SIZE);
 // Open file for reading.
 var fd = fs.openSync('file.txt', 'r');
 
-// Read 10 bytes into buffer, specifying write/read offsets to be 0.
-//
-// JSLint FP: none
-// JSHint FP: none
-// Closure Compiler FP: none
+/* Read 10 bytes into buffer, specifying write/read offsets to be 0.
+ *
+ * JSLint FP: none
+ * JSHint FP: none
+ * Closure Compiler FP: none
+ *
+ * Note: The tools do not flag a warning for a flawed version of this code
+ * (that is, reading more data into the buffer than fits), but executing the 
+ * flawed version gives a RangeError runtime exception.
+ */
+
 var bytesRead = fs.readSync(fd, buffer, 0, BUFFER_SIZE, 0);
