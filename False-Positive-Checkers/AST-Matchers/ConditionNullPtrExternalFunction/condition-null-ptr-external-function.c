a501e6b6f61f45a2bcb21f549a431f238cc8c81e
Abhi <abjayant@iu.edu>
2017-01-11 03:35:14 -0500

    Added Condition-Null-Ptr-External-Function pattern checker

---
+++ False-Positive-Checkers/AST-Matchers/ConditionNullPtrExternalFunction/condition-null-ptr-external-function.c
@@ -0,0 +1,27 @@
+// Juliet s02/CWE590_Free_Memory_Not_on_Heap__delete_class_alloca_11.cpp
+// Structure: conditional-null-ptr-external-function
+
+// *** REQUIRES TEST CASE SUPPORT FILES ***
+
+#include "std_testcase.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int * myint = NULL;
+    if(globalReturnsTrue())
+    {
+        myint = (int *)malloc(sizeof(int));
+        *myint = 5;
+    }
+
+    // CodeSonar FP: none
+    // scan-build FP: (warning) Dereference of null pointer (loaded from variable 'myint')
+    // Cppcheck FP: (error) Possible null pointer dereference: 'myint'
+    printf("int: %d\n", *myint);
+    
+    free(myint);
+    return 0;
+}
---
+++ False-Positive-Checkers/AST-Matchers/ConditionNullPtrExternalFunction/run_clang-tool.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../build/bin/condition-null-ptr-external-function condition-null-ptr-external-function.c --
