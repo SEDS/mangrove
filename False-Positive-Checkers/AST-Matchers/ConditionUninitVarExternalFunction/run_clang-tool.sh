ab750643d4b3624a869ca08171d3609b89c25cc1
Abhi <abjayant@iu.edu>
2017-01-16 10:35:50 -0500

    Added Juliet testcasesupport path

--- False-Positive-Checkers/AST-Matchers/ConditionUninitVarExternalFunction/run_clang-tool.sh
+++ False-Positive-Checkers/AST-Matchers/ConditionUninitVarExternalFunction/run_clang-tool.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-../build/bin/condition-uninit-var-external-function condition-uninit-var-external-function.cpp --
+../build/bin/condition-uninit-var-external-function condition-uninit-var-external-function.cpp -- \
+   -I /path/to/Juliet_Test_Suite/testcasesupport
