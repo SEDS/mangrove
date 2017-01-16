a7ad5dca428e21b4858371413904bdda161fd800
Abhi <abjayant@iu.edu>
2017-01-16 10:23:09 -0500

    Corrected the file extension

--- False-Positive-Checkers/AST-Matchers/ConditionUninitVarExternalVar/run_clang-tool.sh
+++ False-Positive-Checkers/AST-Matchers/ConditionUninitVarExternalVar/run_clang-tool.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../build/bin/condition-uninit-var-external-var condition-uninit-var-external-var.c --
+../build/bin/condition-uninit-var-external-var condition-uninit-var-external-var.cpp --
