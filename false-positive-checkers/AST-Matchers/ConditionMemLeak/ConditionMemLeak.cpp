943dc8ed43bb423767e5ad4dacc26c2c2c5a92fc
Abhi <abjayant@iu.edu>
2017-02-03 18:10:39 -0500

    Modified AST matcher

--- false-positive-checkers/AST-Matchers/ConditionMemLeak/ConditionMemLeak.cpp
+++ false-positive-checkers/AST-Matchers/ConditionMemLeak/ConditionMemLeak.cpp
@@ -17,7 +17,6 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/CommandLine.h"
 
-
 using namespace clang::ast_matchers;
 using namespace std;
 using namespace clang::driver;
@@ -29,65 +28,71 @@ string fileName;
 const Stmt *IfS1;
 const Stmt *IfS2;
 int enter_bit = 1;
+const NamedDecl *var_free;
 
 static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 
 // AST Matcher expressions to match the FP pattern
-// Matches an 'if' statement which has a memory free in it's body
-StatementMatcher IfMatcher = ifStmt(hasDescendant(compoundStmt(hasDescendant(callExpr(hasDescendant(declRefExpr(to(functionDecl(hasName("free")))))))))).bind("ifStmt");
+// Matches an 'if' statement which has a memory free or delete in it's body
+StatementMatcher IfMatcher = ifStmt(hasDescendant(compoundStmt( anyOf(hasDescendant(callExpr(hasAnyArgument(ignoringParenImpCasts(declRefExpr(to(varDecl().bind("var_free"))))), hasDescendant(declRefExpr(to(functionDecl(hasName("free")))))) ), hasDescendant(cxxDeleteExpr()) )))).bind("ifStmt");
 
-// Matches an 'if' statement which has a constatn global variable as it's condition expression
-StatementMatcher if_global_const = ifStmt(hasCondition(implicitCastExpr(has(declRefExpr(to(varDecl(hasGlobalStorage(), hasType(isConstQualified())))))))).bind("if_glob_const");
+
+// Matches an 'if' statement which has a global variable as it's condition expression
+StatementMatcher if_global_const = ifStmt(anyOf(hasCondition(ignoringParenImpCasts(declRefExpr(to(varDecl( anyOf((hasGlobalStorage(), hasInitializer(integerLiteral(unless(equals(0))))), (hasGlobalStorage(), hasExternalFormalLinkage())) ))))), hasCondition(binaryOperator(has(ignoringParenImpCasts(declRefExpr(to(varDecl( anyOf((hasGlobalStorage(), hasInitializer(integerLiteral(unless(equals(0))))), (hasGlobalStorage(), hasExternalFormalLinkage())) ))))))))).bind("if_glob_const");
 
 // Callback for the AST matchers
-class PatternFinder : public MatchFinder::MatchCallback {
+class PatternFinder : public MatchFinder::MatchCallback
+{
     public :
-        virtual void run(const MatchFinder::MatchResult &Result) {
-
-        Context = Result.Context;
-
-        // Isolate the node identified by the AST matcher named 'IfMatcher'
-        if(const Stmt *var1 = Result.Nodes.getNodeAs<clang::Stmt>("if_glob_const"))
+        virtual void run(const MatchFinder::MatchResult &Result)
         {
-            if(Result.Context->getSourceManager().isWrittenInMainFile(var1->getLocStart()))
+            Context = Result.Context;
+
+            // Isolate the node identified by the AST matcher named 'IfMatcher'
+            if(const Stmt *var1 = Result.Nodes.getNodeAs<clang::Stmt>("if_glob_const"))
             {
-                IfS1 = var1;
+                if(Result.Context->getSourceManager().isWrittenInMainFile(var1->getLocStart()))
+                {
+                    IfS1 = var1;
+                }
+            }
+            // Isolate the node identified by the AST matcher named 'if_global_const'
+            if(const Stmt *var2 = Result.Nodes.getNodeAs<clang::Stmt>("ifStmt"))
+            {
+                if(Result.Context->getSourceManager().isWrittenInMainFile(var2->getLocStart()))
+                {
+                    IfS2 = var2;
+                }
+            }
+            // If both the statement matchers match the same statement we conclude that the FP pattern has been found
+            if(areSameExpr(Context,IfS1,IfS2) && enter_bit == 1)
+            {
+                errs() << "\n\n" << fileName << "\n";
+                errs() << "FP Located" << "\n";
+                IfS1 = 0;
+                IfS2 = 0;
+                enter_bit = 0;
+                return;
             }
         }
 
-        // Isolate the node identified by the AST matcher named 'if_global_const'
-        if(const Stmt *var2 = Result.Nodes.getNodeAs<clang::Stmt>("ifStmt"))
+        // Function to check if the expressions are the same
+        static bool areSameExpr(ASTContext *Context, const Stmt *First, const Stmt *Second)
         {
-            IfS2 = var2;
+            if (!First || !Second)
+                return false;
+            llvm::FoldingSetNodeID FirstID, SecondID;
+            First->Profile(FirstID, *Context, true);
+            Second->Profile(SecondID, *Context, true);
+            return FirstID == SecondID;
         }
-        // If both the statement matchers match the same statement we conclude that the FP pattern has been found
-        if(areSameExpr(Context,IfS1,IfS2) && enter_bit == 1)
-        {
-            errs() << "\n\n" << fileName << "\n";
-            errs() << "FP Located" << "\n";
-
-            IfS1 = 0;
-            IfS2 = 0;
-            enter_bit = 0;
-            return;
-        }
-    }
-
-    // Function to check if the expressions are the same
-    static bool areSameExpr(ASTContext *Context, const Stmt *First, const Stmt *Second) {
-        if (!First || !Second)
-            return false;
-        llvm::FoldingSetNodeID FirstID, SecondID;
-        First->Profile(FirstID, *Context, true);
-        Second->Profile(SecondID, *Context, true);
-        return FirstID == SecondID;
-    }
 
     private:
         ASTContext *Context;
 };
 
-int main(int argc, const char **argv) {
+int main(int argc, const char **argv)
+{
     CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
     ClangTool Tool(OptionsParser.getCompilations(), OptionsParser.getSourcePathList());
     fileName = argv[1];
