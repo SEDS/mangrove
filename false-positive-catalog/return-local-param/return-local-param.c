2e5f47557d5b5a53f8f7214f99c5ad1857a83a5e
Zachary <zpreynol@iupui.edu>
2017-05-15 16:17:46 -0400

    Cleaned up code formatting in return-local-param.c

--- false-positive-catalog/return-local-param/return-local-param.c
+++ false-positive-catalog/return-local-param/return-local-param.c
@@ -1,17 +1,14 @@
 // Juliet CWE665_Improper_Initialization__char_cat_21.c
 // Structure: return-local-param
 
-char * helper(char * data)
-{
-	// CodeSonar FP: none
-	// scan-build FP: none
-	// Cppcheck FP: (error) Pointer to local array variable returned.
+char * helper(char * data) {
+    // CodeSonar FP: none
+    // scan-build FP: none
+    // Cppcheck FP: (error) Pointer to local array variable returned.
     return data;
 }
 
-
-int main(void)
-{
+int main(void) {
     char * data;
     char dataBuffer[10];
     data = dataBuffer;
